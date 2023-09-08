ccd5b0d5b446f0f689dd7d3bd929924c966de187
KOREAVC <KOREAVC@DESKTOP-DPE8FLP>
2023-09-08 18:14:59 +0900

    9월 8일 강의

---
+++ P0908/src/JA0908_11.java
@@ -0,0 +1,13 @@
+
+public class JA0908_11 {
+
+	public static void main(String[] args) {
+		//total메소드를 호출해서 사용함
+		//클래스명.메소드명 == static 
+		System.out.println(	JA0908_10_메소드예제2.total(10,10,9));
+		System.out.println(	JA0908_10_메소드예제2.avg(10,10,9));
+		
+
+	}
+
+}
