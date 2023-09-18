ee1684a42363e3684d9cc6fd832405ad871939f3
KOREAVC <KOREAVC@DESKTOP-DPE8FLP>
2023-09-18 15:16:06 +0900

    9/18일 강의

--- P0915_학생성적/src/JA0915_02_학생성적.java
+++ P0915_학생성적/src/JA0915_02_학생성적.java
@@ -22,11 +22,50 @@ public class JA0915_02_학생성적 {
 			case 2:// 2. 학생성적출력메소드
 				stuAction.stuOutput();
 				break;
-			case 3:
-				
-				
-				break;
-				
+			case 3: // 3.학생성적수정메소드
+				System.out.println();
+				System.out.println("찾고자 하는 학생이름을 입력하세요.(0.이전페이지");
+				String name = scan.next();
+				if (name.equals("0")) { // 이전페이지로 이동
+					System.out.println(">>이전페이지로 이동합니다!!");
+					System.out.println();
+					break;
+				} // if
+
+				// 손승민
+				int count = 0;
+				for (int i = 0; i < stuAction.list.size(); i++) {
+					StuScore s = (StuScore) stuAction.list.get(i);
+					if (name.equals(s.getName())) {
+						count = i;
+						System.out.printf("%s 학생을 찾았습니다.\n", name);
+						System.out.println("[ 수정과목 선택 ]");
+						System.out.println("1. 국어");
+						System.out.println("2. 영어");
+						System.out.println("3. 수학");
+						System.out.println("=================================================");
+						System.out.println("원하는 번호를 선택하세요.");
+						choice = scan.nextInt();
+
+						switch (choice) {
+						case 1:
+							System.out.println("[ 국어점수 ]");
+							System.out.println("=============================================");
+							System.out.println("현재점수: " + s.getKor());
+							System.out.println("변경할 점수를 입력하세요");
+							s.setKor(scan.nextInt());
+							s.setTotal(s.getKor() + s.getEng() + s.getMath());
+							s.setAvg(s.getTotal() / 3.0);
+							System.out.println("국어점수가 변경되었습니다.");
+							System.out.println();
+
+							break;
+
+						}// switch
+
+					} // for
+				} // for
+
 			}// switch
 
 		} // while
